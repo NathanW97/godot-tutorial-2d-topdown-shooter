449a824e627de35673e7540bac0c70e2afcf2952
NathanW97 <ngw@live.com.au>
2025-11-03 21:17:41 +1100

    Add gate and billboard scenes, implement player action cooldowns
    Introduces new Gate and Billboard objects with associated scenes and scripts. Updates the level scene to include the Gate and connect its Area2D signals. Enhances player_action.gd to add cooldowns for primary and secondary actions using timers, and updates player.tscn to include these timers and their signal connections.

---
+++ scenes/levels/level.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func _on_area_2d_body_entered(_body: Node2D) -> void:
+	print("Body has entered")
+
+
+func _on_area_2d_body_exited(_body: Node2D) -> void:
+	print("Body has exited")
--- scenes/levels/player_action.gd
+++ scenes/levels/player_action.gd
@@ -1,17 +1,22 @@
 extends Node
 
 var body: CharacterBody2D
+var canPrimary: bool = true
+var canSeconday: bool = true
 var desiredPrimaryAction: bool = false
 var desiredSecondaryAction: bool = false
+@export_range(0,2,0.1) var cooldownPrimary: float = 0.5
+@export_range(0,4,0.5) var cooldownSecondary: float = 2
+
 
 func _ready() -> void:
 	body = get_parent()  # CharacterBody2D of the "Player"
 
 func _process(_delta: float) -> void:
-	if (Input.is_action_just_pressed("primary_fire")):
+	if (Input.is_action_just_pressed("primary_fire") and canPrimary):
 		desiredPrimaryAction = true
 	
-	if (Input.is_action_just_pressed("secondary_fire")):
+	if (Input.is_action_just_pressed("secondary_fire") and canSeconday):
 		desiredSecondaryAction = true
 
 
@@ -20,11 +25,15 @@ func _physics_process(_delta: float) -> void:
 		print("Shoot_P")
 		primaryFire()
 		desiredPrimaryAction = false
+		canPrimary = false
+		$PrimaryTimer.start(cooldownPrimary)
 	
 	if (desiredSecondaryAction):
 		print("Shoot_S")
 		secondaryFire()
 		desiredSecondaryAction = false
+		canSeconday = false
+		$SecondaryTimer.start(cooldownSecondary)
 
 
 func primaryFire() -> void:
@@ -33,3 +42,11 @@ func primaryFire() -> void:
 
 func secondaryFire() -> void:
 	pass
+
+
+func _on_primary_timer_timeout() -> void:
+	canPrimary = true
+
+
+func _on_secondary_timer_timeout() -> void:
+	canSeconday = true
